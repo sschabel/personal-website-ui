9fe57ab255dcf22a70e975d00d5d0f589cc8f136
Sam Schabel <schabelsam@gmail.com>
2024-02-15 11:13:26 -0500

    Removed bearer token from global store since it is no longer needed.

--- src/app/ngrx/global.store.ts
+++ src/app/ngrx/global.store.ts
@@ -3,14 +3,12 @@ import { User } from '@models/user';
 import { patchState, signalStore, withHooks, withMethods, withState } from '@ngrx/signals';
 
 type GlobalState = {
-  bearerToken: string | null;
   lastError: Error | null;
   menuItems: SimpleMenuItem[];
   user: User | null;
 };
 
 const initialState: GlobalState = {
-  bearerToken: null,
   lastError: null,
   menuItems: [],
   user: null
@@ -20,10 +18,6 @@ export const GlobalStore = signalStore(
   { providedIn: 'root' },
   withState(initialState),
   withMethods((store) => ({
-
-    updateBearerToken(bearerToken: string | null) {
-      patchState(store, { bearerToken: bearerToken });
-    },
 
     updateLastError(error: Error | null) {
       patchState(store, { lastError: error });
